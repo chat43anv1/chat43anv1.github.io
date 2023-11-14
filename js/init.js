262e84538b29e109a9f70b1047e9d858720eb3f7
chat43anv1 <150833757+chat43anv1@users.noreply.github.com>
2023-11-14 17:28:54 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyA8RriQJJtfl4MxbKtSwl1uBxmH48clyF0",
-  authDomain: "chatborrado-89c70.firebaseapp.com",
-  projectId: "chatborrado-89c70",
-  storageBucket: "chatborrado-89c70.appspot.com",
-  messagingSenderId: "1002843686378",
-  appId: "1:1002843686378:web:6d2dfed2f6e207c82a5e93",
-  measurementId: "G-2GNRVBFBK2"
+       apiKey: "AIzaSyAy48q8Eyma13_xGXwBVXdQriEnf0OOq5M",
+    authDomain: "chat43anv-d4c43.firebaseapp.com",
+    projectId: "chat43anv-d4c43",
+    storageBucket: "chat43anv-d4c43.appspot.com",
+    messagingSenderId: "996489211912",
+    appId: "1:996489211912:web:4bd1365d501e3948c121bd",
+    measurementId: "G-BRGNVRPVED"
     });
